d925a555b94cba82031b2f76973f6124027b2821
Gabriel Paquin <gpaquin@webagecorp.com>
2012-10-16 22:46:24 +0800

    adding capistrano recipes

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/check"
+load "config/recipes/nodejs"
+
+server "192.168.1.106", :web, :app, :db, primary: true
+
+set :git_enable_submodules, 1
+
+set :application, "redmine"
+set :user, "deployer"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+# this keeps a copy of the Git repository on the server cache 
+# so that it can be updated without having to clone the 
+# repository each time we deploy the application
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:Gabey/gitorious-.git"
+set :branch, "deploy"
+
+# option so that the password prompt works
+default_run_options[:pty] = true
+# authorizes the server to access the Git repository 
+# so that we don’t have to add a deploy key to Github
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" #keep only the last 5 releases
